927208ab190e07abb7ac1c04d3e695a78b250a9b
Talon <talontalon24@gmail.com>
2019-10-03 01:39:54 +0200

    Enhance server appearance and function

--- sql_sankey_server.py
+++ sql_sankey_server.py
@@ -2,7 +2,6 @@
 
 import json
 
-# from flask import Flask, session, redirect, url_for, request, jsonify, render_template
 from flask import Flask
 from flask import render_template
 from flask import request
@@ -13,24 +12,28 @@ app = Flask(__name__)  # pylint: disable=invalid-name
 
 
 @app.route("/", methods=["GET"])
-def onlyform():
+def welcome_page():
     """Welcome page"""
-    return """
-        <form method="post">
-            <p>Query <textarea rows="40" cols="80" name=Query></textarea>
-            <p><input type=submit value=Visualize>
-        </form>
-    """
+    return render_template("welcome.html")
 
 
 @app.route("/", methods=["POST"])
-def index():
-    """Base."""
+def sankey_page():
+    """Attempt to make the diagram, go back if impossible."""
     data = request.form["Query"]
-    data = json.loads(data)
-    graph_json = sql_sankey.generate(data, to_json=True)
+    try:
+        data = json.loads(data)
+    except json.decoder.JSONDecodeError:
+        return render_template(
+            "welcome.html", errors=["Invalid json!"], data=data)
+    try:
+        graph_json = sql_sankey.generate(data, to_json=True)
+    except KeyError:
+        return render_template(
+            "welcome.html", errors=["Explain plan flavour is not supported!"],
+            data=data)
     return render_template(
-        'index.html',
+        'diagram.html',
         ids=["Graph"],
         graphJSON=graph_json)
 
