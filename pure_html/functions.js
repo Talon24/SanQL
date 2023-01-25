d1c410613c233e6384fecc14b229732d905ede2d
Talon <talontalon24@gmail.com>
2023-01-26 00:15:45 +0100

    Semicolons everywhere in js

--- pure_html/functions.js
+++ pure_html/functions.js
@@ -1,78 +1,78 @@
 function selectedMode() {
     for (thing of document.getElementsByName("arrangement")) {
         if ((isVisible(thing) && thing.checked)) {
-            return thing.value
+            return thing.value;
         }
     }
     return null
 }
 
 function isHidden(el) {
-    return (el.offsetParent === null)
+    return (el.offsetParent === null);
 }
-function isVisible(el) { return !isHidden(el) }
+function isVisible(el) { return !isHidden(el); }
 
 function timeformat(millisec) {
-    secs = millisec / 1000
-    hours = Math.floor(secs / 60 / 60)
-    mins = Math.floor(secs / 60 % 60)
-    secs = Math.floor(secs % 60)
-    milli = Math.round(millisec / 10 % 100)
-    hours = hours.toString().padStart(2, 0)
-    mins = mins.toString().padStart(2, 0)
-    secs = secs.toString().padStart(2, 0)
-    milli = milli.toString().padStart(2, 0)
-    out = "{}:{}:{}.{}".format(hours, mins, secs, milli)
-    return out
+    secs = millisec / 1000;
+    hours = Math.floor(secs / 60 / 60);
+    mins = Math.floor(secs / 60 % 60);
+    secs = Math.floor(secs % 60);
+    milli = Math.round(millisec / 10 % 100);
+    hours = hours.toString().padStart(2, 0);
+    mins = mins.toString().padStart(2, 0);
+    secs = secs.toString().padStart(2, 0);
+    milli = milli.toString().padStart(2, 0);
+    out = "{}:{}:{}.{}".format(hours, mins, secs, milli);
+    return out;
 }
 
 function sizeformat(num, suffix = 'B') {
     for (unit of ['', 'Ki', 'Mi', 'Gi', 'Ti', 'Pi', 'Ei', 'Zi']) {
         if (Math.abs(num) < 1024.0) {
-            return "{} {}{}".format(num.toFixed(1), unit, suffix)
+            return "{} {}{}".format(num.toFixed(1), unit, suffix);
         }
-        num /= 1024.0
+        num /= 1024.0;
     }
-    return "{} {}{}".format(num.toFixed(1), 'Yi', suffix)
+    return "{} {}{}".format(num.toFixed(1), 'Yi', suffix);
 }
 
 function prettify_details(data) {
-    var data = JSON.parse(JSON.stringify(data))
-    keylength = 0
-    vallength = 0
+    var data = JSON.parse(JSON.stringify(data));
+    keylength = 0;
+    vallength = 0;
     for (key in data) {
         if (key.toString().startsWith("__")) {
-            delete data[key]
-            continue
+            delete data[key];
+            continue;
         }
         if (Number.isInteger(data[key]) || (Number(data[key]) === data[key] && (data[key] % 1 !== 0))) {
             // Int or float
-            intpart = Math.floor(data[key])
-            fracpart = Math.round(data[key] * 100 % 100)
-            data[key] = "{}.{}".format(intpart.toString(), fracpart.toString().padStart(2, "0"))
+            intpart = Math.floor(data[key]);
+            fracpart = Math.round(data[key] * 100 % 100);
+            data[key] = "{}.{}".format(intpart.toString(), fracpart.toString().padStart(2, "0"));
         }
-        keylength = Math.max(keylength, key.toString().length)
-        vallength = Math.min(Math.max(vallength, data[key].toString().length), 200)
-        data[key] = data[key].toString().match(/.{1,200}/g)  // Split into blocks of up to 200 chars
+        keylength = Math.max(keylength, key.toString().length);
+        vallength = Math.min(Math.max(vallength, data[key].toString().length), 200);
+        data[key] = data[key].toString().match(/.{1,200}/g);  // Split into blocks of up to 200 chars
         // if (typeof data[key] === "object"){
-        //     data[key] = [JSON.stringify(data[key])]
+        //     data[key] = [JSON.stringify(data[key])];
         // }
         // else {
-        //     data[key] = data[key].toString().match(/.{1,200}/g)
+        //     data[key] = data[key].toString().match(/.{1,200}/g);
         // }
     }
-    out = []
-    out.push("╔" + "═".repeat(keylength) + "╦" + "═".repeat(vallength) + "╗")
+    out = [];
+    out.push("╔" + "═".repeat(keylength) + "╦" + "═".repeat(vallength) + "╗");
     for (key in data) {
         for (index in data[key]) {
-            curstr = "║"
-            curstr += key.toString().padEnd(keylength)
-            curstr += "║"
-            curstr += data[key][index].toString().padStart(vallength)
-            curstr += "║"
-            out.push(curstr)
+            curstr = "║";
+            curstr += key.toString().padEnd(keylength);
+            curstr += "║";
+            curstr += data[key][index].toString().padStart(vallength);
+            curstr += "║";
+            out.push(curstr);
         }
     }
-    out.push("╚" + "═".repeat(keylength) + "╩" + "═".repeat(vallength) + "╝")
-    return out.join("<br />")
+    out.push("╚" + "═".repeat(keylength) + "╩" + "═".repeat(vallength) + "╝");
+    return out.join("<br />");
 }
--- pure_html/walkers.js
+++ pure_html/walkers.js
@@ -1,115 +1,119 @@
-function * walker(tree) {
+function* walker(tree) {
     if (Array.isArray(tree)) {
-        yield * postgres_walker(tree[0]["Plan"])
+        yield* postgres_walker(tree[0]["Plan"]);
     } else if ("query_block" in tree) {
-        yield * mysql_walker(tree["query_block"])
+        yield* mysql_walker(tree["query_block"]);
     } else {
-        yield * vertica_walker(tree)
+        yield* vertica_walker(tree);
     }
 }
 
-function * postgres_walker(tree, parent=null, depth=0) {
-    var current = JSON.parse(JSON.stringify(tree))
-    delete current["Plans"]
+function* postgres_walker(tree, parent = null, depth = 0) {
+    var current = JSON.parse(JSON.stringify(tree));
+    delete current["Plans"];
     if ("Actual Total Time" in current) {
-        current["__cost"] = current["Actual Total Time"]
+        current["__cost"] = current["Actual Total Time"];
     } else {
-        current["__cost"] = current["Total Cost"]
+        current["__cost"] = current["Total Cost"];
     }
-    var name = current["Node Type"]
+    var name = current["Node Type"];
     if (name == "CTE Scan" && "CTE Name" in current) {
-        name += " as {}".format(current["CTE Name"])
+        name += " as {}".format(current["CTE Name"]);
     } else if (("Alias" in current)) {
-        name += " as {}".format(current["Alias"])
+        name += " as {}".format(current["Alias"]);
     } else if (("Subplan Name" in current)) {
-        name += " for {}".format(current["Subplan Name"])
+        name += " for {}".format(current["Subplan Name"]);
     }
     if (depth == 0) {
-        // name += " - Total Cost: {:15,.2f}".format(current["__cost"])
-        name += " - Total Cost: {}".format(current["__cost"].toFixed(2))
+        // name += " - Total Cost: {:15,.2f}".format(current["__cost"]);
+        name += " - Total Cost: {}".format(current["__cost"].toFixed(2));
     }
-    current["__label"] = name
+    current["__label"] = name;
     // console.log(current["__cost"]);
 
     for (entry in current) {
         if (entry == "Time") {
-            current[entry] = timeformat(current[entry])
+            current[entry] = timeformat(current[entry]);
         }
     }
     for (entry in current) {
         if (entry == "Space Used") {
-            space = current[entry]  // Postgres returns space in KB
-            current[entry] = "{} KiB".format(space)
+            var space = current[entry];  // Postgres returns space in KB
+            current[entry] = "{} KiB".format(space);
             if (space >= 1024) {
-                current[entry] += " - Approx. {}".format(sizeformat(space*1000))
+                current[entry] += " - Approx. {}".format(sizeformat(space * 1000));
             }
         }
     }
-    yield [current, parent, depth]
+    yield [current, parent, depth];
     if ("Plans" in tree) {
         for (plan of tree["Plans"]) {
-            yield * postgres_walker(plan, current, depth + 1)
+            yield* postgres_walker(plan, current, depth + 1);
         }
     }
 }
 
-function prunejoin(dict_, list_, sep=", "){
-out = []
+function prunejoin(dict_, list_, sep = ", ") {
+    out = []
     for (key of list_) {
         if (key in dict_) {
-            out.push(dict_[key])
+            out.push(dict_[key]);
         }
     }
-    return out.join(sep)
+    return out.join(sep);
 }
 
-function * vertica_walker(tree, parent=null, depth=0) {
-    var current = JSON.parse(JSON.stringify(tree))
-    delete current["INPUT"]
-    delete current["Inner"]
-    delete current["Outer"]
-    delete current["MATERIALIZE"]
-    delete current["MATERIALIZE_AT_OUPUT"]
-    current["__cost"] = current["COST"]
-    name = prunejoin(current, ["PATH_NAME", "EXTRA"], sep=" - ")
-    current["__label"] = name
-    yield [current, parent, depth]
+function* vertica_walker(tree, parent = null, depth = 0) {
+    var current = JSON.parse(JSON.stringify(tree));
+    delete current["INPUT"];
+    delete current["Inner"];
+    delete current["Outer"];
+    delete current["MATERIALIZE"];
+    delete current["MATERIALIZE_AT_OUPUT"];
+    current["__cost"] = current["COST"];
+    var name = prunejoin(current, ["PATH_NAME", "EXTRA"], sep = " - ");
+    current["__label"] = name;
+    yield [current, parent, depth];
     if ("INPUT" in tree) {
-        yield * vertica_walker(tree["INPUT"], current, depth + 1)}
+        yield* vertica_walker(tree["INPUT"], current, depth + 1);
+    }
     if ("Inner" in tree) {
-        yield * vertica_walker(tree["Inner"], current, depth + 1)}
+        yield* vertica_walker(tree["Inner"], current, depth + 1);
+    }
     if ("Outer" in tree) {
-        yield * vertica_walker(tree["Outer"], current, depth + 1)}
+        yield* vertica_walker(tree["Outer"], current, depth + 1);
+    }
 }
 
-function * mysql_walker(tree, parent=null, depth=0) {
-    var current = JSON.parse(JSON.stringify(tree))
-    delete current["nested_loop"]
-    delete current["used_columns"]
-    costs = 0
+function* mysql_walker(tree, parent = null, depth = 0) {
+    var current = JSON.parse(JSON.stringify(tree));
+    delete current["nested_loop"];
+    delete current["used_columns"];
+    var costs = 0;
     for (key in current["cost_info"]) {
-        current["Cost - " + key] = current["cost_info"][key]
+        current["Cost - " + key] = current["cost_info"][key];
         // if (key.endsWith("cost")) {
         //     costs += Number(current["cost_info"][key])
         // }
     }
-    if ("cost_info" in current && "query_cost" in current["cost_info"]) { costs = Number(current["cost_info"]["query_cost"])}
-    if ("read_cost" in current["cost_info"]) { costs += Number(current["cost_info"]["read_cost"])}
-    current["__cost"] = costs
-    delete current["cost_info"]
-    name = current["table_name"]
+    if ("cost_info" in current && "query_cost" in current["cost_info"]) { costs = Number(current["cost_info"]["query_cost"]); }
+    if ("read_cost" in current["cost_info"]) { costs += Number(current["cost_info"]["read_cost"]); }
+    current["__cost"] = costs;
+    delete current["cost_info"];
+    var name = current["table_name"];
     if (depth == 0) {
-        name += " - Total Cost: {}".format(current["__cost"].toFixed(2))
+        name += " - Total Cost: {}".format(current["__cost"].toFixed(2));
     }
-    current["__label"] = name
-    delete current["ordering_operation"]
-    delete current["duplicates_removal"]
-    yield [current, parent, depth]
-    container = tree
-    if ("ordering_operation" in container) {container = container["ordering_operation"]}
-    if ("duplicates_removal" in container) {container = container["duplicates_removal"]}
-    if ("nested_loop" in container){
+    current["__label"] = name;
+    delete current["ordering_operation"];
+    delete current["duplicates_removal"];
+    yield [current, parent, depth];
+    var container = tree;
+    if ("ordering_operation" in container) { container = container["ordering_operation"]; }
+    if ("duplicates_removal" in container) { container = container["duplicates_removal"]; }
+    if ("nested_loop" in container) {
         for (entry of container["nested_loop"]) {
-            yield * mysql_walker(entry["table"], current, depth + 1)}
+            yield* mysql_walker(entry["table"], current, depth + 1);
+        }
     }
 }
